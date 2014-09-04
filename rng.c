9caf1828e7a1b6b52e8ec6386dee30862815778d
Brant Robertson <brant.robertson@gmail.com>
2014-09-03 22:40:16 -0700

    Wrapper functions for using GSL rngs.
    A set of wrapper functions for using the GSL random number
    distributions.

---
+++ rng.c
@@ -0,0 +1,300 @@
+/*! \file rng.c
+ *  \brief Function definitions for easy random number generation */
+#include"rng.h"
+#include<gsl/gsl_rng.h>
+#include<gsl/gsl_randist.h>
+
+/*! \var int flag_rng_gaussian
+ *  \brief Initialization flag for Gaussian RNG */
+int flag_rng_gaussian;
+
+/*! \var int flag_rng_uniform
+ *  \brief Initialization flag for uniform RNG */
+int flag_rng_uniform;
+
+/*! \var int flag_rng_exponential
+ *  \brief Initialization flag for exponential RNG */
+int flag_rng_exponential;
+
+/*! \var int flag_rng_direction
+ *  \brief Initialization flag for direction RNG */
+int flag_rng_direction;
+
+/*! \var int flag_rng_integer
+ *  \brief Initialization flag for direction RNG */
+int flag_rng_integer;
+
+/*! \var int flag_rng_permutation
+ *  \brief Initialization flag for permutation RNG*/
+int flag_rng_permutation;
+
+
+/*! \var const gsl_rng_type *T_rng_gaussian
+ * \brief RNG type for Gaussian PDF */
+const gsl_rng_type *T_rng_gaussian;
+
+/*! \var const gsl_rng_type *T_rng_uniform;
+ * \brief RNG type for uniform PDF */
+const gsl_rng_type *T_rng_uniform;
+
+/*! \var const gsl_rng_type *T_rng_exponential;
+ * \brief RNG type for exponential PDF */
+const gsl_rng_type *T_rng_exponential;
+
+/*! \var const gsl_rng_type *T_rng_direction;
+ * \brief RNG type for direction PDF */
+const gsl_rng_type *T_rng_direction;
+
+/*! \var const gsl_rng_type *T_rng_integer;
+ * \brief RNG type for integer PDF */
+const gsl_rng_type *T_rng_integer;
+
+
+/*! \var const gsl_rng_type *T_rng_permutation;
+ * \brief RNG type for permutation PDF */
+const gsl_rng_type *T_rng_permutation;
+
+/*! \var gsl_rng *r_rng_gaussian
+ *  \brief Gaussian RNG */
+gsl_rng *r_rng_gaussian;
+
+/*! \var gsl_rng *r_rng_uniform
+ *  \brief Uniform RNG */
+gsl_rng *r_rng_uniform;
+
+/*! \var gsl_rng *r_rng_exponential
+ *  \brief Exponential RNG */
+gsl_rng *r_rng_exponential;
+
+/*! \var gsl_rng *r_rng_integer
+ *  \brief Integer RNG */
+gsl_rng *r_rng_integer;
+
+/*! \var gsl_rng *r_rng_direction
+ *  \brief Direction RNG */
+gsl_rng *r_rng_direction;
+
+/*! \var gsl_rng *r_rng_permutation
+ *  \brief Permutation RNG */
+gsl_rng *r_rng_permutation;
+
+/*! \var gsl_permuation *p_rng_permutation
+ *  \brief Permutation */
+gsl_permutation *p_rng_permutation;
+
+/*! \fn double rng_gaussian(double mu, double sigma)
+ *  \brief Returns a Gaussianly-distributed random number with mean mu and dispersion sigma */
+double rng_gaussian(double mu, double sigma)
+{
+	if(flag_rng_gaussian!=1337)
+		initialize_rng_gaussian();
+	return gsl_ran_gaussian(r_rng_gaussian, sigma) + mu;
+}
+
+/*! \fn void initialize_rng_gaussian(void)
+ *  \brief Initializes Gaussian random number generator */
+void initialize_rng_gaussian(void)
+{
+	flag_rng_gaussian = 1337;
+
+	gsl_rng_env_setup();
+
+	T_rng_gaussian = gsl_rng_default;
+	r_rng_gaussian = gsl_rng_alloc(T_rng_gaussian);
+	
+}
+
+/*! \fn double rng_uniform(double a, double b)
+ *  \brief Returns a uniformly-distributed random number between a and b*/
+double rng_uniform(double a, double b)
+{
+	if(flag_rng_uniform!=1337)
+		initialize_rng_uniform();
+	return gsl_ran_flat(r_rng_uniform, a, b);
+}
+
+/*! \fn double rng_exponential(double mu)
+ *  \brief Returns a exponentially-distributed random number with scale mu*/
+double rng_exponential(double mu)
+{
+	if(flag_rng_exponential!=1337)
+		initialize_rng_exponential();
+	return gsl_ran_exponential(r_rng_exponential, mu);
+}
+
+/*! \fn size_t *rng_permutation(int n)
+ *  \brief Returns [0,n-1], randomly permutated*/
+size_t *rng_permutation(int n)
+{
+	size_t *permutation;
+	if(flag_rng_permutation!=1337)
+		initialize_rng_permutation(n);
+
+	if(flag_rng_permutation==1337)
+		if(n!=p_rng_permutation->size)
+			initialize_rng_permutation(n);
+	
+
+	//permutation = calloc_size_t_array(n);
+
+	//for(int i=0;i<n;i++)
+	//	permutation[i] = i;
+
+	//gsl_permute(p_rng_permutation, permutation, 1, n);
+
+	//return permutation;
+
+	gsl_ran_shuffle(r_rng_permutation,p_rng_permutation->data, n, sizeof(size_t));
+	return gsl_permutation_data(p_rng_permutation);
+}
+
+/*! \fn void initialize_rng_permutation(void)
+ *  \brief Initializes integer random permutation generator*/
+void initialize_rng_permutation(int n)
+{
+	if(flag_rng_permutation!=1337)
+	{
+		flag_rng_permutation = 1337;
+		gsl_rng_env_setup();
+		T_rng_permutation = gsl_rng_taus;
+		r_rng_permutation = gsl_rng_alloc(T_rng_permutation);
+
+		p_rng_permutation = gsl_permutation_alloc(n);
+	}else{
+		if(n!=p_rng_permutation->size)
+		{
+			gsl_permutation_free(p_rng_permutation);
+			p_rng_permutation = gsl_permutation_alloc(n);
+		}
+	}
+
+	gsl_permutation_init(p_rng_permutation);
+	gsl_ran_shuffle(r_rng_permutation,p_rng_permutation->data, n, sizeof(size_t));
+
+}
+
+
+/*! \fn int rng_integer(int n)
+ *  \brief Returns a uniformly-distributed random number between a and b*/
+int rng_integer(int n)
+{
+	if(flag_rng_integer!=1337)
+		initialize_rng_integer();
+	return gsl_rng_uniform_int(r_rng_integer, n);
+}
+
+
+/*! \fn void initialize_rng_integer(void)
+ *  \brief Initializes integer random number generator */
+void initialize_rng_integer(void)
+{
+	flag_rng_integer = 1337;
+
+	gsl_rng_env_setup();
+
+	T_rng_integer = gsl_rng_taus;
+	r_rng_integer = gsl_rng_alloc(T_rng_integer);
+}
+
+
+/*! \fn void initialize_rng_uniform(void)
+ *  \brief Initializes uniform random number generator */
+void initialize_rng_uniform(void)
+{
+	flag_rng_uniform = 1337;
+
+	gsl_rng_env_setup();
+
+	//T_rng_uniform = gsl_rng_default;
+	T_rng_uniform = gsl_rng_taus;
+	r_rng_uniform = gsl_rng_alloc(T_rng_uniform);
+}
+
+/*! \fn void initialize_rng_exponential(void)
+ *  \brief Initializes exponential random number generator */
+void initialize_rng_exponential(void)
+{
+	flag_rng_exponential = 1337;
+
+	gsl_rng_env_setup();
+
+	T_rng_exponential = gsl_rng_taus;
+	r_rng_exponential = gsl_rng_alloc(T_rng_exponential);
+}
+
+/*! \fn void initialize_rng_direction(void)
+ *  \brief Initializes direction random number generator */
+void initialize_rng_direction(void)
+{
+	flag_rng_direction = 1337;
+
+	gsl_rng_env_setup();
+
+	//T_rng_direction = gsl_rng_default;
+	T_rng_direction = gsl_rng_taus;
+	r_rng_direction = gsl_rng_alloc(T_rng_direction);
+}
+
+
+/*! \fn void set_rng_uniform_seed(int seed)
+ *  \brief Set uniform rng seed */
+void set_rng_uniform_seed(int seed)
+{
+	if(flag_rng_uniform!=1337)
+		initialize_rng_uniform();
+	gsl_rng_set(r_rng_uniform, seed);
+}
+
+/*! \fn void set_rng_exponential_seed(int seed)
+ *  \brief Set exponential rng seed */
+void set_rng_exponential_seed(int seed)
+{
+	if(flag_rng_exponential!=1337)
+		initialize_rng_exponential();
+	gsl_rng_set(r_rng_exponential, seed);
+}
+
+/*! \fn void set_rng_gaussian_seed(int seed)
+ *  \brief Set gaussian rng seed */
+void set_rng_gaussian_seed(int seed)
+{
+	if(flag_rng_gaussian!=1337)
+		initialize_rng_gaussian();
+	gsl_rng_set(r_rng_gaussian, seed);
+}
+
+/*! \fn void set_rng_direction_seed(int seed)
+ *  \brief Set direction rng seed */
+void set_rng_direction_seed(int seed)
+{
+	if(flag_rng_direction!=1337)
+		initialize_rng_direction();
+	gsl_rng_set(r_rng_direction, seed);
+}
+
+/*! \fn double *rng_direction(int ndim)
+ *  \brief Get a random unit vector */
+double *rng_direction(int ndim)
+{
+	double *x;
+	if(ndim<2)
+	{
+		printf("ndim must be > 1\n");
+		return NULL;
+	}
+	//x = calloc_double_array(ndim);
+	if(!(x = (double *) calloc(ndim,sizeof(double))))
+	{
+		printf("Error allocating x of size %d\n",ndim);
+		return NULL;
+	}
+	switch(ndim)
+	{
+		case 2: gsl_ran_dir_2d(r_rng_direction,&x[0],&x[1]);
+			break;
+		case 3: gsl_ran_dir_3d(r_rng_direction,&x[0],&x[1],&x[2]);
+			break;
+		default: gsl_ran_dir_nd(r_rng_direction,ndim,x);
+	}
+	return x;
+}
---
+++ rng.h
@@ -0,0 +1,98 @@
+#include<gsl/gsl_rng.h>
+#include<gsl/gsl_permutation.h>
+#ifndef BRANT_RNG
+#define BRANT_RNG
+
+extern int flag_rng_gaussian;
+extern int flag_rng_uniform;
+extern int flag_rng_exponential;
+extern int flag_rng_direction;
+extern int flag_rng_integer;
+extern int flag_rng_permutation;
+
+extern const gsl_rng_type *T_rng_gaussian;
+extern const gsl_rng_type *T_rng_uniform;
+extern const gsl_rng_type *T_rng_exponential;
+extern const gsl_rng_type *T_rng_direction;
+extern const gsl_rng_type *T_rng_integer;
+extern const gsl_rng_type *T_rng_permutation;
+
+extern gsl_rng *r_rng_gaussian;
+extern gsl_rng *r_rng_uniform;
+extern gsl_rng *r_rng_exponential;
+extern gsl_rng *r_rng_direction;
+extern gsl_rng *r_rng_integer;
+extern gsl_rng *r_rng_permutation;
+
+extern gsl_permutation *p_rng_permutation;
+
+/*! \fn double rng_gaussian(double mu, double sigma)
+ *  \brief Returns a Gaussianly-distributed random number with mean mu and dispersion sigma */
+double rng_gaussian(double mu, double sigma);
+
+/*! \fn void initialize_rng_gaussian(void)
+ *  \brief Initializes Gaussian random number generator */
+void initialize_rng_gaussian(void);
+
+/*! \fn int rng_uniform(int n)
+ *  \brief Returns a random integer between 0 and n-1*/
+int rng_integer(int n);
+
+/*! \fn double rng_uniform(double a, double b)
+ *  \brief Returns a uniformly-distributed random number between a and b*/
+double rng_uniform(double a, double b);
+
+/*! \fn double rng_exponential(double mu)
+ *  \brief Returns a exponentially-distributed random number with scale mu*/
+double rng_exponential(double mu);
+
+/*! \fn double *rng_direction(int ndim)
+ *  \brief Returns a random direction in n dimensions*/
+double *rng_direction(int ndim);
+
+/*! \fn size_t *rng_permutation(int N)
+ *  \brief Returns [0,N-1] randomly permutated*/
+size_t *rng_permutation(int N);
+
+
+/*! \fn void initialize_rng_uniform(void)
+ *  \brief Initializes uniform random number generator */
+void initialize_rng_uniform(void);
+
+/*! \fn void initialize_rng_exponential(void)
+ *  \brief Initializes exponential random number generator */
+void initialize_rng_exponential(void);
+
+/*! \fn void initialize_rng_direction(void)
+ *  \brief Initializes direction random number generator */
+void initialize_rng_direction(void);
+
+/*! \fn void initialize_rng_integer(void)
+ *  \brief Initializes integer random number generator */
+void initialize_rng_integer(void);
+
+/*! \fn void initialize_rng_permutation(void)
+ *  \brief Initializes permutation generator */
+void initialize_rng_permutation(int n);
+
+
+/*! \fn void set_rng_integer_seed(int seed)
+ *  \brief Set integer rng seed. */
+void set_rng_integer_seed(int seed);
+
+/*! \fn void set_rng_uniform_seed(int seed)
+ *  \brief Set uniform rng seed. */
+void set_rng_uniform_seed(int seed);
+
+/*! \fn void set_rng_exponential_seed(int seed)
+ *  \brief Set exponential rng seed. */
+void set_rng_exponential_seed(int seed);
+
+/*! \fn void set_rng_gaussian_seed(int seed)
+ *  \brief Set gaussian rng seed. */
+void set_rng_gaussian_seed(int seed);
+
+/*! \fn void set_rng_direction_seed(int seed)
+ *  \brief Set direction rng seed. */
+void set_rng_direction_seed(int seed);
+#endif //BRANT_RNG
